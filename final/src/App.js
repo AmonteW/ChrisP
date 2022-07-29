bf39ffed9ea649d6612d4edd35ebc00ebbd2ad2e
AmonteW <106997856+AmonteW@users.noreply.github.com>
2022-07-28 22:18:13 -0500

    no progress

--- final/src/App.js
+++ final/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import SelectionPokedex from './SelectionPokedex';
+import { Pokedex } from 'pokeapi-js-wrapper';
+import React, {useState} from 'react'
 
+const P = new Pokedex.Pokedex()
+const [pokedex, setPokedex] = (null);
+P.resource([
+  "api/v2/pokedex",
+]).then( response => {
+  console.log(response)
+})
+let name = Pokedex.name;
 function App() {
-  return (
-    <div>
-      <header className="App-header">
-      </header>
+console.log("me maw")
+return (
+  <div>
+    <SelectionPokedex pokedex={pokedex}></SelectionPokedex>
+    <button>{name}</button>
     </div>
-  );
+);
 }
 
 export default App;
---
+++ final/src/SelectionPokedex.jsx
@@ -0,0 +1,12 @@
+
+function SelectionPokedex() {
+    return (
+        <div>
+            pokedex.map((cb)
+            <li>{P.name}</li>
+            <button>view</button>
+            )
+        </div>
+    )
+}
+export default SelectionPokedex;
